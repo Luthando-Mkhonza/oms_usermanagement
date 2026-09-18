219d2b74dc145f058670ab21a9f92002c85d1e91
Luthando-Mkhonza <teddyluthando125@gmail.com>
2026-09-18 10:33:52 +0200

    add user entity

---
+++ src/main/java/fnb/oms_usermanagement/entity/User.java
@@ -0,0 +1,43 @@
+package fnb.oms_usermanagement.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+@Entity
+@Table(name="users")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class User {
+
+    private long id;
+
+    private String firstName;
+
+    private String surname;
+
+    private String email;
+
+    private Role role;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+        updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
+}
